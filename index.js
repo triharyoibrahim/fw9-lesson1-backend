b64e2dc0e6509aa5255e000027becffd21604f21
triharyoibrahim <rizkitriharyoibrahim@gmail.com>
2022-09-28 09:36:04 +0700

    add some code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
